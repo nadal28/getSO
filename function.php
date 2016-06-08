7d49303d30a43e7f2900c47b88ce71b44bd55ff3
nadal28 <nadal_28@hotmail.es>
2016-06-08 08:42:59 +0200

    Create function.php

---
+++ function.php
@@ -0,0 +1,24 @@
+function getSO(&$user_agent){
+	$plataformas = array(
+		'Windows 10' => 'Windows NT 10.0+',
+		'Windows 8.1' => 'Windows NT 6.3+',
+		'Windows 8' => 'Windows NT 6.2+',
+		'Windows 7' => 'Windows NT 6.1+',
+		'Windows Vista' => 'Windows NT 6.0+',
+		'Windows XP' => 'Windows NT 5.1+',
+		'Windows 2003' => 'Windows NT 5.2+',
+		'Windows' => 'Windows otros',
+		'iPhone' => 'iPhone',
+		'iPad' => 'iPad',
+		'Mac OS X' => '(Mac OS X+)|(CFNetwork+)',
+		'Mac otros' => 'Macintosh',
+		'Android' => 'Android',
+		'BlackBerry' => 'BlackBerry',
+		'Linux' => 'Linux',
+		);
+	foreach($plataformas as $plataforma=>&$pattern){
+		if(preg_match('/'.$pattern.'/i', $user_agent))
+			return $plataforma;
+	}
+	return 'Otro';
+}
